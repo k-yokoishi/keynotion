b227358fd6153873415a4ffd962b7f21a4caeb86
k-yokoishi <ss.xs.m713@gmail.com>
2023-03-28 00:08:24 +0900

    fix: Fix logic to get millisec from title

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,9 +1,9 @@
 export const getMilliseconds = (text: string): number | null => {
-  const matchedMinutes = text.match(/([0-9]+)min/)
+  const matchedMinutes = text.match(/(\d+)\s?(min|minute)s?/)
   if (matchedMinutes) {
     return matchedMinutes !== null ? parseInt(matchedMinutes[1]) * 60 * 1000 : null
   }
-  const matchedSeconds = text.match(/([0-9]+)sec/)
+  const matchedSeconds = text.match(/(\d+)\s?(sec|second)s?/)
   if (matchedSeconds) {
     return matchedSeconds !== null ? parseInt(matchedSeconds[1]) * 1000 : null
   }
